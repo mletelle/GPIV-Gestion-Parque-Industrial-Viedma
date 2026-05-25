3538a11bfef909aad39a0c353bc984c15933a81f
LauPerisse <123lautiperisse123@gmail.com>
2026-05-25 20:43:26 -0300

    fix: placeholder del campo login actualizado a 'Usuario o Correo Electrónico'

--- core/forms.py
+++ core/forms.py
@@ -12,7 +12,7 @@ class LoginForm(AuthenticationForm):
     username = forms.CharField(
         widget=forms.TextInput(attrs={
             'class': 'form-control',
-            'placeholder': 'Usuario',
+            'placeholder': 'Usuario o Correo Electrónico',
             'autofocus': True,
         })
     )
